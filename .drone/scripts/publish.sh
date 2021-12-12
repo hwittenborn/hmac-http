4d94eeb4ad19785b1ea43641d4ea195a291fc97d
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-12-12 02:46:34 -0600

    Fixed ordering of commands in CI tag and publishing scripts
    We need to add our SSH key *before* 'get-ssh-key' runs, as it won't be able to set the perms for that file otherwise.

--- .drone/scripts/publish.sh
+++ .drone/scripts/publish.sh
@@ -2,15 +2,17 @@
 set -e
 mpr_fingerprint='SHA256:TQtnFwjBwpDOHnHTaANeudpXVmomlYo6Td/8T51FA/w'
 
-SSH_HOST="${mpr_url}" \
-SSH_EXPECTED_FINGERPRINT="${mpr_fingerprint}" \
-SET_PERMS="true" \
-get-ssh-key
+mkdir -p "${HOME}/.ssh/"
 
 echo "${ssh_key}" > "${HOME}/.ssh/ssh_key"
 echo "Host ${mpr_url}" > "/${HOME}/.ssh/config"
 echo "  Hostname ${mpr_url}" > "/${HOME}/.ssh/config"
 echo "  IdentityFile /${HOME}/.ssh/ssh_key" | tee -a "/${HOME}/.ssh/config"
+
+SSH_HOST="${mpr_url}" \
+SSH_EXPECTED_FINGERPRINT="${mpr_fingerprint}" \
+SET_PERMS="true" \
+get-ssh-key
 
 git clone "ssh://mpr@${mpr_url}/python3-hmac-http"
 cp makedeb/PKGBUILD python3-hmac-http/PKGBUILD
