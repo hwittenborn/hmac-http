89140e3d889067c3f19453d8937ed844d4ef7724
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-12-12 00:21:18 -0600

    Added command to update APT cache in CI publish step

--- .drone/scripts/publish.sh
+++ .drone/scripts/publish.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/bash
 set -ex
+
+apt-get update
 apt-get install curl jq -y
+
 pkgver="$(cat .data.json | jq -r '.version')"
+
 pip install build
 python -m build
+
 curl "https://${proget_server}/pypi/python/upload/hmac-http-${pkgver}.tar.gz" \
      --user "api:${proget_api_key}" \
      --upload-file "./dist/hmac-http-${pkgver}.tar.gz"
